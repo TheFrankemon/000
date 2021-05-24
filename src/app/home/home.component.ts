45c8ee52f112335f328b40dad0805d12b27bf87f
TheFrankemon <francomontiel@hotmail.com>
2021-05-24 01:35:42 -0400

    Beta version of side-by-side header
    Add neon animations
    Add title w/ neon effect & font
    Add neon bars & basic-colored bars with matching slope values
    Add lorem ipsum about section & photo parallax bg
    Add enhanced mousehandler fn

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  screenWidth = 0;
+  scrPercentLeft = 50;
+  scrPercentRight = 50;
+  wait = false;
+  tps = 10;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.screenWidth = window.screen.availWidth;
+  }
+
+  @HostListener("mousemove", ['$event.clientX'])
+  enhancedMouseHandler($event: number) {
+    if (!this.wait) {
+      const mouseX = $event;
+      const mouseAtPercent = Math.round((mouseX / this.screenWidth) * 100);
+      this.scrPercentLeft = mouseAtPercent;
+      this.scrPercentRight = 100 - mouseAtPercent;
+
+      this.wait = true;
+      setTimeout(() => {
+        this.wait = false;
+      }, 1000/ this.tps);
+    }
   }
 }
