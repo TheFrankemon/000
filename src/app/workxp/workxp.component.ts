070a7f2d89a353d68f07d54bed86a4550d999483
TheFrankemon <francomontiel@hotmail.com>
2022-10-25 00:11:53 -0400

    Implement /static/ experience view

---
+++ src/app/workxp/workxp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workxp',
+  templateUrl: './workxp.component.html',
+  styleUrls: ['./workxp.component.scss']
+})
+export class WorkxpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
