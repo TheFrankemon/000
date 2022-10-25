2f3518457325f9cb3bc02a92b625e2a709bb7a54
TheFrankemon <francomontiel@hotmail.com>
2022-10-25 00:11:42 -0400

    Temp? remove black section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,34 +7,9 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  screenWidth = 0;
-  scrPercentLeft = 50;
-  scrPercentRight = 50;
-  wait = false;
-  tps = 10;
-
   constructor() { }
 
   ngOnInit(): void {
-    this.screenWidth = window.screen.availWidth;
   }
 
-  @HostListener("mousemove", ['$event.clientX'])
-  enhancedMouseHandler($event: number) {
-    if (!this.wait) {
-      const mouseX = $event;
-      const mouseAtPercent = Math.round((mouseX / this.screenWidth) * 100);
-      if (mouseAtPercent < 10) {
-        this.scrPercentLeft = 95;
-      } else if (mouseAtPercent > 90) {
-        this.scrPercentLeft = 5;
-      }
-      this.scrPercentRight = 100 - this.scrPercentLeft;
-
-      this.wait = true;
-      setTimeout(() => {
-        this.wait = false;
-      }, 1000/ this.tps);
-    }
-  }
 }
