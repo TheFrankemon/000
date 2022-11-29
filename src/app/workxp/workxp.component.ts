4d7fef35dd0b6dcd61dbe229a6a7583868b6a127
TheFrankemon <francomontiel@hotmail.com>
2022-11-29 00:51:00 -0400

    Optimize dynamic work/studies iterator

--- src/app/workxp/workxp.component.ts
+++ src/app/workxp/workxp.component.ts
@@ -7,6 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkxpComponent implements OnInit {
 
+  jobs = [{
+    id: 'witco',
+    title: 'Witco (formerly Monbuilding)',
+    dates: 'Sep 2018 - Present',
+    detail: [
+      'Lead Web UI Developer on Sass + Angular.',
+      'Frontend Developer on Angular.'
+    ]
+  }, {
+    id: 'ancestral',
+    title: 'Ancestral Gods',
+    dates: 'Jan 2018 - Jun 2018',
+    detail: [
+      'Videogame developer trainee on Unity3D & C# programming.'
+    ]
+  }, {
+    id: 'impeller',
+    title: 'Impeller Studios',
+    dates: 'Jul 2016 - Jul 2017',
+    detail: [
+      'Co-leader of local web videogame development team, using Phaser and team training on Git.'
+    ]
+  }, {
+    id: 'assure',
+    title: 'AssureSoft',
+    dates: 'Jan 2017 - Feb 2017',
+    detail: [
+      'Research Assistant intern on a CI tools POC.',
+      'Angular Material 2 web components development.'
+    ]
+  }]
+
+  studies = [{
+    id: 'upb',
+    title: 'Universidad Privada Boliviana',
+    dates: 'Feb 2013 - Dec 2018',
+    firstElemIsGold: true,
+    detail: [
+      'Computer Software Engineer.',
+      'Honorable mention on directed-work thesis.',
+      'Career representative on University\'s 2016 student society.'
+    ]
+  }, {
+    id: 'cba',
+    title: 'Centro Boliviano Americano',
+    dates: 'Jan 2004 - Feb 2008',
+    detail: [
+      'Certificate of Intermediate-Superior Program for Teenagers.',
+      'Completion Diploma of Children’s English Program.',
+    ]
+  }]
+
   skills = [{
     id: 'angular',
     title: 'Angular 2-14',
