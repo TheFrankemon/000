e5894b50075ffbcd82671e667b37b538450d395a
TheFrankemon <francomontiel@hotmail.com>
2023-07-09 23:32:13 -0400

    Base passport items UI

--- src/app/passport/passport.component.ts
+++ src/app/passport/passport.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PassportComponent implements OnInit {
 
+  categories = [{
+    name: 'Countries',
+    bgImage: 'countries.jpg'
+  }, {
+    name: 'PS Trophies',
+    bgImage: 'trophies.jpg'
+  }, {
+    name: 'Music Library',
+    bgImage: 'music.jpg'
+  }, {
+    name: 'Food',
+    bgImage: 'food.jpg'
+  }]
+
   constructor() { }
 
   ngOnInit(): void {
