d4de51ef7ff629bd2558f9a8fa1a951251bf12de
TheFrankemon <francomontiel@hotmail.com>
2024-05-27 02:20:50 -0400

    Add country labels and hover effect
    Refactor hardcoded countries into dynamic array

--- src/app/passport/countries/countries.component.ts
+++ src/app/passport/countries/countries.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-countries',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountriesComponent implements OnInit {
 
-  constructor() { }
+  visitedCountries: {
+    name: string
+    code: string
+    sanitizedUrl?: SafeUrl
+  }[];
+  hovered: number;
 
-  ngOnInit(): void {
+  constructor(
+    private sanitizer: DomSanitizer
+  ) {
+    this.visitedCountries = [{
+      name: "Argentina",
+      code: "ar"
+    }, {
+      name: "Bolivia",
+      code: "bo"
+    }, {
+      name: "Brazil",
+      code: "br"
+    }, {
+      name: "Chile",
+      code: "cl"
+    }, {
+      name: "Egypt",
+      code: "eg"
+    }, {
+      name: "Japan",
+      code: "jp"
+    }, {
+      name: "Peru",
+      code: "pe"
+    }, {
+      name: "Spain",
+      code: "es"
+    }, {
+      name: "Turkey",
+      code: "tr"
+    }, {
+      name: "USA",
+      code: "us"
+    }]
+
+    this.hovered = -1;
   }
 
+  ngOnInit(): void {
+    this.visitedCountries = this.visitedCountries.map(item => ({
+      ...item,
+      sanitizedUrl: this.sanitizeURL(`assets/countries/${item.code}.svg`)
+    }));
+  }
+
+  sanitizeURL(url: string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  test() {
+    alert('sdada');
+  }
 }
