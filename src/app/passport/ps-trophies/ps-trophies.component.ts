cf5792107331a876839adc1ff15fb2348e87fcaf
TheFrankemon <francomontiel@hotmail.com>
2025-10-04 20:40:13 -0400

    Create trophies summary component

--- src/app/passport/ps-trophies/ps-trophies.component.ts
+++ src/app/passport/ps-trophies/ps-trophies.component.ts
@@ -8,8 +8,9 @@ import { TrophiesType } from 'src/app/types';
   styleUrls: ['./ps-trophies.component.scss']
 })
 export class PsTrophiesComponent implements OnInit {
-
   trophiesData: TrophiesType[] = [];
+  trophiesSummary = {platinum: 0, gold: 0, silver: 0, bronze: 0};
+  trophiesLevel = {pslevel: 218, total: 0};
   loading = true;
 
   constructor(
@@ -18,11 +19,24 @@ export class PsTrophiesComponent implements OnInit {
 
   async ngOnInit() {
     this.trophiesData = await this.jsonHttpService.getAllTrophies();
+    this.trophiesSummary = this.trophiesData.reduce((acc, game) => {
+      acc.platinum += game.trophies.platinum;
+      acc.gold += game.trophies.gold;
+      acc.silver += game.trophies.silver;
+      acc.bronze += game.trophies.bronze;
+      return acc;
+    }, this.trophiesSummary);
+    this.trophiesLevel = this.trophiesData.reduce((acc, game) => {
+      acc.total += game.trophies.platinum + game.trophies.gold + game.trophies.silver + game.trophies.bronze;
+      return acc;
+    }, this.trophiesLevel);
+
     this.loading = false;
   }
 
-  getTrophiesObj(gameTrophies: {platinum: number, gold: number, silver: number, bronze: number; }) {
-    return Object.entries(gameTrophies);
+  getTrophiesObj(gameTrophies: TrophiesType['trophies']) {
+    const order: (keyof TrophiesType['trophies'])[] = ['platinum', 'gold', 'silver', 'bronze'];
+    return order.map(key => [key, gameTrophies[key]]);
   }
 
 }
